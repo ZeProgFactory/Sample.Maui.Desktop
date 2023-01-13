39811ecddd10bbd3a3bcf0c1c7cce4af3d01aef7
Michael <Michael@ZPF.fr>
2023-01-13 16:13:49 +0100

    Initial Checkin ...

---
+++ MauiDesktopApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiDesktopApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiDesktopApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDesktopApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
